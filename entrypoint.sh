081610f74da7303aaa62bd8ff4b9ee44dab4cbce
salman <slmn.sttr.65@gmail.com>
2024-11-28 01:33:24 +0500

    updated script

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,31 +11,19 @@ curl https://dl.min.io/client/mc/release/linux-amd64/mc \
 
 chmod +x $HOME/minio-binaries/mc
 export PATH=$PATH:$HOME/minio-binaries/
-  
+
 STORAGE_SERVICE_ALIAS=${STORAGE_SERVICE_ALIAS:="s3"}
 STORAGE_SERVICE_URL=${STORAGE_SERVICE_URL:="https://s3.amazonaws.com"}
-AWS_SESSION_TOKEN=${AWS_SESSION_TOKEN:=""}
-AWS_REGION=${AWS_REGION:=""}
+FILENAME="dump_$(date +%Y%m%d%H%M%S)"
+ACCESS_KEY_ID=${ACCESS_KEY_ID:=""}
+SECRET_ACCESS_KEY=${SECRET_ACCESS_KEY:=""}
+AWS_BUCKET=${AWS_BUCKET:=""}
 MONGODB_URI=${MONGODB_URI:=""}
-MONGODB_NAME=${MONGODB_NAME:=""}
 
-
-# Check mirror target is set
-if [ -z "$MIRROR_TARGET" ]; then
-  echo "MIRROR_TARGET is not set"
-  exit 1
-fi
-
-# Set mc configuration
-if [ -z "$AWS_SESSION_TOKEN" ]; then
-  mc alias set "$STORAGE_SERVICE_ALIAS" "$STORAGE_SERVICE_URL" "$ACCESS_KEY_ID" "$SECRET_ACCESS_KEY"
-else
-  export MC_HOST_${STORAGE_SERVICE_ALIAS}=https://${ACCESS_KEY_ID}:${SECRET_ACCESS_KEY}:${AWS_SESSION_TOKEN}@s3.${AWS_REGION}.amazonaws.com
-fi
-
+mc alias set "$STORAGE_SERVICE_ALIAS" "$STORAGE_SERVICE_URL" "$ACCESS_KEY_ID" "$SECRET_ACCESS_KEY"
 
 # Execute mc pipe mongodump output to s3 bucket and check for failure
-mongodump --archive --oplog --uri=$MONGODB_URI | mc pipe "$STORAGE_SERVICE_ALIAS/$MIRROR_TARGET/$MONGODB_NAME"
+mongodump --archive --gzip --uri=$MONGODB_URI | mc pipe "$STORAGE_SERVICE_ALIAS/$AWS_BUCKET/$FILENAME"
 MONGODUMP_STATUS=$?
 
 if [ $MONGODUMP_STATUS -ne 0 ]; then
